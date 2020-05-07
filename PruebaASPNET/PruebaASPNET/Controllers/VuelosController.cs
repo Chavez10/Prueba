f342b020b4d4f9b62756286ba1d8ec225c79ba72
Chavez10 <davidchavez1098@outlook.com>
2020-05-06 18:03:03 -0600

    Se agregaron los controladores Pagos y Vuelos

---
+++ PruebaASPNET/PruebaASPNET/Controllers/VuelosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaASPNET.Controllers
+{
+    public class VuelosController : Controller
+    {
+        // GET: Vuelos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
